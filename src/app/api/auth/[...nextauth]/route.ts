a12fb5165c82aa8a66eb1b2a6e0b59a5cfc12dd5
ImGr0Ot <anthonyperezpantaleon@gmail.com>
2024-04-12 14:22:54 -0400

    fixing redirect in callback again (route.ts)

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -34,7 +34,7 @@ const handler = NextAuth({
     },
   }),
 ], 
-  callbacks: { redirect: async () => "/",
+  callbacks: { redirect: async () => "https://restaurant-nextjs-beta.vercel.app/api/auth/callback/google",
   
     async signIn({ user, account}) {
       
