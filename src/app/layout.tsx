4ec2ac938fa9dd109af87e2c24ee2b27d563f9b8
ImGr0Ot <anthonyperezpantaleon@gmail.com>
2024-04-22 18:38:34 -0400

    fixing the favicon (layout)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,49 +1,41 @@
-import type { Metadata } from "next"
-import { Inter } from "next/font/google"
-import "./globals.css"
-import { ToastContainer } from "react-toastify"
-import "react-toastify/dist/ReactToastify.css"
-import { Providers } from "./Providers"
-import NavBar from "./components/NavBar"
-import { Suspense } from "react"
-import Loading from "./loading"
-import { ContextProvider } from "@/app/context/Context"
-import Footer from "./components/Footer"
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { Providers } from "./Providers";
+import NavBar from "./components/NavBar";
+import { Suspense } from "react";
+import Loading from "./loading";
+import { ContextProvider } from "@/app/context/Context";
+import Footer from "./components/Footer";
 
-const inter = Inter({ subsets: ["latin"] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-	title: "Restaurant App",
-	description: "Restaurant App for personal use"}
+  title: "Restaurant App",
+  description: "Restaurant App for personal use",
+};
 
 export default function RootLayout({
-	children,
+  children,
 }: Readonly<{
-	children: React.ReactNode
+  children: React.ReactNode;
 }>) {
-	return (
-		<html lang='en'>
-			<head>	
-			<link rel="icon" href="/favicon.ico" sizes="any" />
-			<link
-				rel="icon"
-				href="/icon?<generated>"
-				type="image/<generated>"
-				sizes="<generated>"
-			/>
-			</head>
-			<body className={inter.className}>
-				<Providers>
-					<ContextProvider>
-						<Suspense fallback={<Loading />}>
-							<NavBar />
-							{children}
-							<Footer />
-						</Suspense>
-					</ContextProvider>
-					<ToastContainer />
-				</Providers>
-			</body>
-		</html>
-	)
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <Providers>
+          <ContextProvider>
+            <Suspense fallback={<Loading />}>
+              <NavBar />
+              {children}
+              <Footer />
+            </Suspense>
+          </ContextProvider>
+          <ToastContainer />
+        </Providers>
+      </body>
+    </html>
+  );
 }
